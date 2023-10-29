3a2e489590ab25e3f8e6e12dffa5e9e057d425e2
Azfar-Sphere <s.azfar.rahman@gmail.com>
2023-10-29 22:26:22 +0400

    gems and store list

---
+++ script.js
@@ -0,0 +1,10 @@
+const gems = [
+    {name: 'ruby', price: 10},
+    {name: 'emerald', price: 20},
+    {name: 'diamond', price: 30},
+];
+const store = [
+    {name: 'ruby', amount: 10},
+    {name: 'emerald', amount: 10},
+    {name: 'diamonds', amount: 10},
+];
