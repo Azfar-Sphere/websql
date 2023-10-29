4bdfb71c0e55d41a638e59a886ba2e521d4ef353
Azfar-Sphere <s.azfar.rahman@gmail.com>
2023-10-29 22:35:15 +0400

    Added function to make an insert data to db

--- script.js
+++ script.js
@@ -4,7 +4,25 @@ const gems = [
     {name: 'diamond', price: 30},
 ];
 const store = [
-    {name: 'ruby', amount: 10},
-    {name: 'emerald', amount: 10},
-    {name: 'diamonds', amount: 10},
+    {gem: 'ruby', amount: 10},
+    {gem: 'emerald', amount: 10},
+    {gem: 'diamonds', amount: 10},
 ];
+
+const db = window.openDatabase('data', '1.0', 'data', 1*1024*1024);
+db.transaction(t => 
+{
+    t.executeSql('CREATE TABLE gems (name TEXT, price INTEGER)');
+    t.executeSql('CREATE TABLE store (gem TEXT, amount INTEGER)');
+
+    for (let g of gems) 
+    {
+        t.executeSql('INSERT INTO gems (name, price) VALUES (?, ?)', [g.name, g.price]);  
+    }
+
+    for (let s of store) 
+    {
+        t.executeSql('INSERT INTO store (name, amount) VALUES (?, ?)', [s.gem, s.amount]);  
+    }
+
+}, e => console.error(e));
